2c38e6f683a991b301481f64cdb55644b8964a8c
KauaBorgesDEV <kaua_borges@gmail.sesisenai.org.br>
2026-03-25 09:08:03 -0300

    feat - finalização

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ let form = document.getElementById('formulario');
 
 form.addEventListener('submit', function (e) {
     e.preventDefault();
-
     let valido = true;
 
     let nome = document.getElementById('nome').value;
@@ -12,4 +11,34 @@ form.addEventListener('submit', function (e) {
     document.getElementById('erro_nome').textContent = '';
     document.getElementById('erro_email').textContent = '';
     document.getElementById('erro_senha').textContent = '';
+    
+    if (nome.length < 3) {
+        document.getElementById('erro_nome').textContent = 'O nome deve conter pelo menos 3 caracteres.';
+
+        valido = false;
+    }
+
+    if (!email.includes('@')) {
+        document.getElementById('erro_email').textContent = 'O email deve conter um "@" válido.';
+        valido = false;
+
+    }
+
+    if (senha.length < 6) {
+        document.getElementById('erro_senha').textContent = 'A senha deve conter pelo menos 6 caracteres no minimo.';
+        valido = false;
+
+    }
+
+    if (valido) {
+        let resultado = document.getElementById('resultado');
+
+        resultado.innerHTML = `
+            Cadastro realizado com sucesso! <br>
+            Nome: ${nome} <br>   
+            email: ${email} <br>
+            Senha: ${senha} <br>
+        `;
+        form.reset();
+    }
 })
